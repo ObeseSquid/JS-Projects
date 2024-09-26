717e86e20510c49bc51805cc82ad0616c5aecdd3
MaxWainwright1 <152036636+ObeseSquid@users.noreply.github.com>
2024-09-26 18:34:53 +0100

    finish basic password generation

---
+++ Password-Generator/main.js
@@ -0,0 +1,113 @@
+const values = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+// Get a random value from the array
+
+function getRandomValue(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// Generate 4 passwords of 15 characters long and display them in the specific id's
+
+function generate() {
+  for (let i = 1; i <= 4; i++) {
+    let password = "";
+    for (let count = 0; count < 15; count++) {
+      const rand = getRandomValue(values);
+      password += rand;
+    }
+    let passwordInfo = document.getElementById("password-box" + i);
+    passwordInfo.textContent = password;
+  }
+}
